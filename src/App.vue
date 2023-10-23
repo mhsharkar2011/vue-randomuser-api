9cdd976b4f636a605f51095b199450bb17cd7fcb
monirbd <monirbd.me@gmail.com>
2023-10-23 23:21:45 +0600

    Vue install

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,6 @@
+<script setup>
+</script>
 
 <template>
-  <h1>Hello World</h1>
+<h1>Hello</h1>
 </template>
-
-
-  <script>
-    export default{
-      name:'App',
-      component:{},
-    }
-  </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
