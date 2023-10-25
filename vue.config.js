11575ca5d16cbf29dd844774719d1631c328b8f5
monirbd <monirbd.me@gmail.com>
2023-10-25 09:13:42 +0600

    Router setup

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    devServer: {
+      proxy: {
+        '^/api': {
+          target: 'http://localhost:5173',
+          changeOrigin: true,
+          logLevel: 'debug',
+          pathRewrite: { '^/api': '/' },
+        },
+      },
+    },
+  }
